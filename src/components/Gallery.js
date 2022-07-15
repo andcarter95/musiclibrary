5c332cf84e419ef96d33269729c9c042a548480d
andcarter95 <and.carter95@gmail.com>
2022-07-14 19:35:57 -0500

    Jake is a bully

---
+++ src/components/Gallery.js
@@ -0,0 +1,21 @@
+import { useContext } from 'react'
+import { DataContext } from '../context/DataContext'
+import GalleryItem from './GalleryItem'
+
+const Gallery = (props) => {
+
+    const display = props.data.map((item, index) => {
+        return (
+            <GalleryItem key={index} item={item} />
+        )
+    })
+
+    return (
+        <div>
+            {display}
+        </div>
+    )
+}
+
+
+export default Gallery
